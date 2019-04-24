643d30038bfbc0571682e2d2e3399723d34f33f8
Ian Smith <ianmsmith17@gmail.com>
2019-04-24 14:03:16 -0500

    got weather data to return, throwing unhandled promise rejection error

--- server.js
+++ server.js
@@ -10,30 +10,32 @@ const cors = require('cors');
 
 app.use(cors());
 
+let locationData = {};
+let weatherData = {};
+
 // CREATE LOCATION ROUTE
 app.get('/location', (request, response) => {
   // STORE THE USER'S QUERY-TURNED-LOCATION-OBJECT IN LOCATIONDATA
   searchToLatLong(request, response);
+  response.send(locationData);
 });
 
 // CREATE WEATHER ROUTE
 app.get('/weather', (request, response) => {
   // STORE THE USER'S QUERY LOCATION 
-  getWeather();
+  getWeather(searchToLatLong(request, response), response);
 });
 
 // CREATE A NEW LOCATION OBJECT FOR THE USER'S QUERY
 const searchToLatLong = function(request, response) {
   let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-  // const location = new Location(query, geoData);
-  // return location;
+
   return superagent.get(url)
     .then(res => {
-      console.log(request.query.data);
-      response.send(new Location(request.query.data, res));
+      locationData = new Location(request.query.data, res);
     }).catch(error => {
-      console.log(error);
-      response.status(500).send("Internal server error");
+      console.log('1');
+      response.status(500).send("Internal Server Error");
     }) 
 };
 
@@ -44,17 +46,24 @@ const searchToLatLong = function(request, response) {
 function Location(query, res) {
   this.query = query;
   this.formatted_query = res.body.results[0].formatted_address;
-  this.latitude = res.body.results[0].geometry.location.lat;
-  this.longitude = res.body.results[0].geometry.location.lng;
+  this.lat = res.body.results[0].geometry.location.lat;
+  this.lng = res.body.results[0].geometry.location.lng;
 }
 
 // RETURN ALL WEATHER RECORDS FOR THE USER'S LOCATION QUERY
-const getWeather = () => {
-  const darkskyData = require('./data/darksky.json');
-  
-  return darkskyData.daily.data.map(day => {
-    return new Weather(day);
-  });
+const getWeather = function(request, response) {
+  //const darkskyData = require('./data/darksky.json');
+  let url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.lat},${request.lng}`;
+  //let url = `https://api.darksky.net/forecast/d014db8157816772e1553d6da93c8fc8/41.8781136,-87.6297982`;
+  return superagent.get(url)
+    .then(res => {
+      response.send(res.body.daily.data.map(day => {
+        return new Weather(day);
+      })).catch(error => {
+      console.log(error);
+      response.status(500).send('Internal Server Error')
+      });
+    });
 }
 
 
